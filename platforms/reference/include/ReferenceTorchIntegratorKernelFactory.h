cbe52976fd88f53fb459db197040e397a675b1f6
SCM <samuel.musson@durham.ac.uk>
2022-05-16 15:17:37 +0100

    Corrected licencing in each file and changed OpenCL default

--- platforms/reference/include/ReferenceTorchIntegratorKernelFactory.h
+++ platforms/reference/include/ReferenceTorchIntegratorKernelFactory.h
@@ -1,36 +1,18 @@
 #ifndef OPENMM_REFERENCETORCHINTEGRATORKERNELFACTORY_H_
 #define OPENMM_REFERENCETORCHINTEGRATORKERNELFACTORY_H_
 
-/* -------------------------------------------------------------------------- *
- *                                   OpenMM                                   *
- * -------------------------------------------------------------------------- *
- * This is part of the OpenMM molecular simulation toolkit originating from   *
- * Simbios, the NIH National Center for Physics-Based Simulation of           *
- * Biological Structures at Stanford, funded under the NIH Roadmap for        *
- * Medical Research, grant U54 GM072970. See https://simtk.org.               *
- *                                                                            *
- * Portions copyright (c) 2014 Stanford University and the Authors.           *
- * Authors: Peter Eastman                                                     *
- * Contributors:                                                              *
- *                                                                            *
- * Permission is hereby granted, free of charge, to any person obtaining a    *
- * copy of this software and associated documentation files (the "Software"), *
- * to deal in the Software without restriction, including without limitation  *
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
- * and/or sell copies of the Software, and to permit persons to whom the      *
- * Software is furnished to do so, subject to the following conditions:       *
- *                                                                            *
- * The above copyright notice and this permission notice shall be included in *
- * all copies or substantial portions of the Software.                        *
- *                                                                            *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
- * THE AUTHORS, CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,    *
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      *
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  *
- * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
- * -------------------------------------------------------------------------- */
+/*---------------------------------------------------------------------------------------------------
+ * Copyright (c) 2022 Samuel C. Musson
+ *
+ * openmmtorchplugin is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ;
+ * either version 2 of the License, or (at your option) any later version.
+ * openmmtorchplugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with openmmtorchplugin ;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*-------------------------------------------------------------------------------- */
 
 #include "openmm/KernelFactory.h"
 
